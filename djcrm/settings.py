a5f0dfb49a01e585f8c69d811ad4c64598f8b86c
Shailendra Singh <shailendrasingh6377@gmail.com>
2026-04-17 13:10:54 +0530

    update allowed hosts

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-crm-ag2h.onrender.com']
 
 
 # Application definition
